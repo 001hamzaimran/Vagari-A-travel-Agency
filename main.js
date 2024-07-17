e225826f7dae4b7f0a1edc9cf6b111c94a153a7e
001hamzaimran <syed.hamza2004@gmail.com>
2024-07-18 00:57:44 +0500

    40% complete

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
-import './style.css'
-import './responsive.css'
+import './style.css';
+import './responsive.css';
+import arrayOfProducts from './products';
+
+function hot_Products() {
+    let parentContainer = document.querySelector(".hot_Products");
+    var clutter ='';
+    arrayOfProducts.forEach(elem => {
+        clutter +=`<div class=" main_hot_product_Wrapper ">
+      <div class="hot_prod_img">
+        <img src=${elem.image} class="img-fluid " alt="">
+      </div>
+      <div class="main_content p-4 ">
+        <div class="content_hot_prod">
+          <h2 class="diplay-4 fw-semibold ">${elem.Name}</h2>
+          <p><span class="icon_span"><i class="fa-solid fa-location-dot" ></i></span>${elem.location}</p>
+        </div>
+        <hr>
+        <div class="hot_prod_desc">
+          <p>${elem.description}</p>
+        </div>
+        <hr>
+        <div class="hot_prod_btn">
+          <button > Detail</button>
+          <div class="hot_prod_price">
+            <h5>FROM</h5>
+            <h3>${elem.rates}</h3>
+          </div>
+        </div>
+      </div>
+    </div>`;
+});
+parentContainer.innerHTML=clutter;
+console.log(parentContainer);
+    
+
+}
 
 
+hot_Products();
---
+++ products.js
@@ -0,0 +1,28 @@
+let arrayOfProducts = [
+    {
+        Name: "Chiang Mai",
+        location: "Thailand",
+        description: "Visit the temples and the Chiang Mai Night Bazaar, a huge huge market located on Chiang Klan Road.",
+        rates:`${420}`,
+        originalRates:`${520}`,
+        image: "public/Assets/Hot_prod_1.jpeg"
+    },
+    {
+        Name: "Nara ",
+        location: "Japan",
+        description: "Discover the incredible landmarks such as the Todai-ji temple and its famous Great Buddha statue.",
+        rates:`${890}`,
+        originalRates:`${999}`,
+        image: "public/Assets/Hot_prod_2.jpeg"
+    },
+    {
+        Name: "Santa Monica",
+        location: "California",
+        description: "This iconic landmark is home to an array of thrilling good attractions and stunning fresh ocean views.",
+        rates:`${650}`,
+        originalRates:`${780}`,
+        image: "public/Assets/Hot_prod_3.jpeg"
+    }
+];
+
+export default arrayOfProducts;
